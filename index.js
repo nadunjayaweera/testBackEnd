b44539b7ed81b27a788cdff48495d952c2592345
nmjayaweera@students.nsbm.lk <nmjayaweera@students.nsbm.ac.lk>
2023-03-28 23:12:40 +0530

    file commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import app from "./server.js"
+import app from "./server.cjs"
 import mongodb from "mongodb"
 import SignupDAO from "./dao/signupDAO.js"
 
--- server.cjs
+++ server.cjs
@@ -1,5 +1,5 @@
-import express from "express"
-import cors from "cors"
+const express = require("express");
+const cors = require("cors");
 import signup from "./api/signup.route.js"
 
 const app = express()
