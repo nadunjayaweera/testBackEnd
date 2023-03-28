ac61ea59b2b358a628fb7d53fb1c5efe5a1e176b
nmjayaweera@students.nsbm.lk <nmjayaweera@students.nsbm.ac.lk>
2023-03-28 18:39:25 +0530

    new update files

---
+++ index.js
@@ -0,0 +1,27 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import SignupDAO from "./dao/signupDAO.js"
+
+const MongoClient = mongodb.MongoClient
+
+const uri = `mongodb+srv://nadunmj:EYO4y6xhMq0PzxDX@cluster0.idbvi1f.mongodb.net/?retryWrites=true&w=majority`
+    // checking pipeline
+const port = 8000
+    // test pipe line
+
+MongoClient.connect(
+        uri, {
+            maxPoolSize: 50,
+            wtimeoutMS: 2500,
+            useNewUrlParser: true
+        })
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    .then(async client => {
+        await SignupDAO.injectDB(client)
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
--- server.js
+++ server.js
@@ -1,43 +1,20 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const app = express();
-const uri = "mongodb+srv://nadunmj:EYO4y6xhMq0PzxDX@cluster0.idbvi1f.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+import express from "express"
+import cors from "cors"
+import signup from "./api/signup.route.js"
 
-let db;
+const app = express()
 
-// connect to the MongoDB Atlas cluster
-client.connect(err => {
-  if (err) {
-    console.log("Error connecting to MongoDB Atlas: ", err);
-  } else {
-    console.log("Connected successfully to MongoDB Atlas");
-    db = client.db("test"); // get a reference to the database
-  }
-});
+app.use(cors())
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use("/api/v1/signup", signup)
+app.use("*", (req, res) => res.status(404).json({error: "not found"}))
 
-// define a route that retrieves data from the database
-app.get('/users', (req, res) => {
-  db.collection('users').find({}).toArray((err, docs) => {
-    if (err) {
-      console.log("Error retrieving data from MongoDB Atlas: ", err);
-      res.status(500).send("Error retrieving data from MongoDB Atlas");
-    } else {
-      console.log("Retrieved data from MongoDB Atlas: ", docs);
-      res.send(docs);
-    }
-  });
-});
+export default app
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
 
 
 // Username- nadunmj
 // Password- EYO4y6xhMq0PzxDX
 // URL- mongodb+srv://nadunmj:<password>@cluster0.idbvi1f.mongodb.net/?retryWrites=true&w=majority
+// npx nodemon app.js
