bd708f85d4a7d0da7b02984f0248280b001af400
nmjayaweera@students.nsbm.lk <nmjayaweera@students.nsbm.ac.lk>
2023-04-07 20:02:08 +0530

    add create date

--- dao/signupDAO.js
+++ dao/signupDAO.js
@@ -15,24 +15,32 @@ export default class UserDAO {
         }
     }
 
-    static async addUser(fname, lname, email, password) {
-        try {
-            const addDoc = {
-                fname: fname,
-                lname: lname,
-                email: email,
-                password: password,
-            }
-            return await users.insertOne(addDoc)
-        } catch (e) {
-            console.error(`Unable to add user: ${e}`)
-            return { error: e }
+static async addUser(fname, lname, email, password) {
+    try {
+        const count = await users.countDocuments();
+        const userId = (count + 1).toString().padStart(3, '0');
+        const currentDate = new Date().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }); // get current date in ISO format
+        const addDoc = {
+            userId: userId,
+            fname: fname,
+            lname: lname,
+            email: email,
+            password: password,
+            createDate: currentDate // add the current date to the document
         }
+        return await users.insertOne(addDoc);
+    } catch (e) {
+        console.error(`Unable to add user: ${e}`);
+        return { error: e };
     }
+}
+
+
 
     static async createUser(userInfo) {
         const { fname, lname, email, password } = userInfo;
         const user = {
+          _id: ObjectId(), // generate unique id for user
           fname: fname,
           lname: lname,
           email: email.toLowerCase(),
